8f3c1cf487d474b4b287ace1add99b2178c2065a
meitaaaa <163484689+meitaaaa@users.noreply.github.com>
2024-08-12 10:40:01 +0700

    Add files via upload

---
+++ Bab9soal3/src/main/java/com/mycompany/bab9soal3/Bab9soal3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab9soal3;
+
+/**
+ *
+ * @author Wpras
+ */
+public class Bab9soal3 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 6) {
+            System.out.println(i);
+            i++; // Tambahkan pernyataan ini untuk menginkremen variabel i
+        }
+    }
+}
+
+    
