8b5aeaf67815e897f921864281f7a1567cbb0fcf
meitaaaa <163484689+meitaaaa@users.noreply.github.com>
2024-08-12 10:57:20 +0700

    Add files via upload

---
+++ Bab18soal1/src/main/java/com/mycompany/bab18soal1/Bab18soal1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab18soal1;
+
+/**
+ *
+ * @author Wpras
+ */
+public class Bab18soal1 {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0; // Ini akan melempar ArithmeticException
+        } catch (ArithmeticException e) {
+            System.out.println("Terjadi kesalahan: " + e.getMessage());
+        }
+    }
+}
+
