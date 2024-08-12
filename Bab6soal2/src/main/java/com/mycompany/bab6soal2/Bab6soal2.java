e956375e09e11e050c6cf4e31dff6c27a092f110
meitaaaa <163484689+meitaaaa@users.noreply.github.com>
2024-08-12 10:29:03 +0700

    Add files via upload

---
+++ Bab6soal2/src/main/java/com/mycompany/bab6soal2/Bab6soal2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab6soal2;
+
+/**
+ *
+ * @author Wpras
+ */
+public class Bab6soal2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
