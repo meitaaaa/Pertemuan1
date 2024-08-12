0242abcc6e499450499dffe2beec2f42ee792add
meitaaaa <163484689+meitaaaa@users.noreply.github.com>
2024-08-12 10:17:31 +0700

    Add files via upload

---
+++ bab3soal2/src/main/java/com/mycompany/bab3soal2/Bab3soal2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab3soal2;
+
+/**
+ *
+ * @author Wpras
+ */
+public class Bab3soal2 {
+
+    public static void main(String[] args) {
+        int NIM = 2110117000;
+        String NamaMahasiswa = "Andika Jaya Utama";
+        String Kelas = "01SIFE020";
+        double Usia = 19.5;
+        Double TinggiBadan = 1.72;
+        
+        System.out.println("NIM : " +NIM);
+        System.out.println("NamaMahasiswa : " +NamaMahasiswa);
+        System.out.println("Kelas : " +Kelas);
+        System.out.println("Usia : " +Usia);
+        System.out.println("TinggiBadan : " +TinggiBadan);
+
+
+
+
+        
+    }
+}
