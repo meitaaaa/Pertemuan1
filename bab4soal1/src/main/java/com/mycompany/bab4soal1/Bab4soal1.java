5961da8f2597cccfa9acfa06717d394acf053376
meitaaaa <163484689+meitaaaa@users.noreply.github.com>
2024-08-12 10:18:24 +0700

    Add files via upload

---
+++ bab4soal1/src/main/java/com/mycompany/bab4soal1/Bab4soal1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab4soal1;
+
+/**
+ *
+ * @author Wpras
+ */
+public class Bab4soal1 {
+
+    public static void main(String[] args) {
+        int Angka1 = 10;
+        int Angka2 = 3;
+        
+        int hasilPertambahan = Angka1 + Angka2;
+        int hasilPengurangan = Angka1 - Angka2;
+        int hasilPerkalian = Angka1 * Angka1;
+        double hasilPembagian = (double) Angka1 / Angka2;
+        
+        System.out.println("a. Hasil Pertambahan A+B = " + hasilPertambahan);
+        System.out.println("b. Hasil Pengurangan A-B = " + hasilPengurangan);
+        System.out.println("c. Hasil Perkalian A*B = " + hasilPerkalian);
+        System.out.println("d. Hasil Pembagian A/B = " + hasilPembagian);
+        
+    }
+}
