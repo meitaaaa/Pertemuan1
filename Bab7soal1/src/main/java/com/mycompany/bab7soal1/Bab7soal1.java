45040892795c278048cc1fb4399d4386da5d71ca
meitaaaa <163484689+meitaaaa@users.noreply.github.com>
2024-08-12 10:30:49 +0700

    Add files via upload

---
+++ Bab7soal1/src/main/java/com/mycompany/bab7soal1/Bab7soal1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab7soal1;
+
+/**
+ *
+ * @author Wpras
+ */
+public class Bab7soal1 {
+
+    public static void main(String[] args) {
+        
+        int day = 2;
+        switch (day) {
+            case 1:
+                System.out.println("Sabtu");
+            case 2:
+            System.out.println("Sabtu");
+            break;
+        }
+                
+    }
+}
