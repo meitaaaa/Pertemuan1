525ee729a4f401a6731950e06ee93efe1d0800bf
meitaaaa <163484689+meitaaaa@users.noreply.github.com>
2024-08-12 10:37:29 +0700

    Add files via upload

---
+++ Bab9soal1/src/main/java/com/mycompany/bab9soal1/Bab9soal1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab9soal1;
+
+/**
+ *
+ * @author Wpras
+ */
+public class Bab9soal1 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            int j = 1;
+            while (j <= i) {
+                System.out.print("* ");
+                j++;
+            }
+            System.out.println(); // baris baru
+            i++;
+        }
+    }
+}
